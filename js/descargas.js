2e4727a22e26797893bcf859cfb1ba072ad92cd4
anthonysilvac <anthony.silvac@gmail.com>
2022-05-28 09:29:18 -0400

    se agrega sweetalert en pagina descargas

---
+++ js/descargas.js
@@ -0,0 +1,25 @@
+let descarga = document.querySelectorAll('.alertDescarga')
+
+
+descarga.forEach(descarga => {
+    descarga.addEventListener('click', () => {
+        Swal.fire({
+            title: '¿Desea descargar nuestro planner de regalo?',
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'si',
+            denyButtonText: `nos`,
+          }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+              Swal.fire('Descargando!', '', 'success')
+            } else if (result.isDenied) {
+              Swal.fire('Cancelado', '', 'info')
+            }
+          })
+    })
+})
+
+
+
+  
